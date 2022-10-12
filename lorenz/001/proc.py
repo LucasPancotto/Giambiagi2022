d87ecc28951dc68f36537f8e9d7c20d8c0333e9d
PatricioClark <patoclark@gmail.com>
2022-10-12 17:20:08 -0300

    Edited procs

--- lorenz/001/proc.py
+++ lorenz/001/proc.py
@@ -2,19 +2,53 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import sys
 
 plt.figure(1)
 out = np.loadtxt('output.dat', unpack=True)
-plt.plot(out[0], out[1])
-plt.plot(out[0], out[2])
+plt.semilogy(out[0], out[1], label='data')
+plt.semilogy(out[0], out[2], label='phys')
+plt.xlabel('Epoch', fontsize=14)
+plt.ylabel('Loss', fontsize=14)
+plt.legend(fontsize=14)
+
+# plt.show()
 
 plt.figure(2)
 true = np.load('../chaotic.npy')
 tt   = true[:, 0]
 true = true[:, 1:4]
 pred = np.load('pred.npy')
+lett = ['x', 'y', 'z']
 for ii in range(3):
     plt.figure(2+ii)
-    plt.plot(tt, true[:, ii])
-    plt.plot(tt, pred[:, ii])
+    plt.plot(tt, true[:, ii], label='truth')
+    plt.plot(tt, pred[:, ii], '--', label='prediction')
+    plt.xlabel('$t$', fontsize=14)
+    plt.ylabel(f'${lett[ii]}$', fontsize=14)
+    plt.legend(fontsize=14)
+# plt.close('all')
+plt.show()
+
+pred = np.load('pred_02000.npy')
+lett = ['x', 'y', 'z']
+for ii in range(3):
+    plt.figure(2+ii)
+    plt.plot(tt, true[:, ii], label='truth')
+    plt.plot(tt, pred[:, ii], '--', label='prediction')
+    plt.xlabel('$t$', fontsize=14)
+    plt.ylabel(f'${lett[ii]}$', fontsize=14)
+    plt.legend(fontsize=14)
+
+plt.figure(3)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.plot3D(true[:, 0], true[:, 1], true[:, 2])
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+
 plt.show()
--- lorenz/002/proc.py
+++ lorenz/002/proc.py
@@ -7,18 +7,24 @@ plt.figure(1)
 out = np.loadtxt('output.dat', unpack=True)
 plt.semilogy(out[0], out[1], label='data')
 plt.semilogy(out[0], out[2], label='phys')
-bal = np.loadtxt('balance.dat', unpack=True)
-plt.semilogy(bal[0], bal[1], label='alpha')
-plt.legend()
+# bal = np.loadtxt('balance.dat', unpack=True)
+# plt.semilogy(bal[0], bal[1], label='alpha')
+plt.legend(fontsize=14)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
 
 true = np.load('../chaotic.npy')
 tt   = true[:, 0]
 true = true[:, 1:4]
 pred = np.load('pred.npy')
+lett = ['x', 'y', 'z']
 for ii in range(3):
     plt.figure(2+ii)
-    plt.plot(tt, true[:, ii])
-    plt.plot(tt, pred[:, ii])
+    plt.plot(tt, true[:, ii], label='truth')
+    plt.plot(tt, pred[:, ii], '--', label='prediction')
+    plt.xlabel('$t$', fontsize=14)
+    plt.ylabel(f'${lett[ii]}$', fontsize=14)
+    plt.legend(fontsize=14)
 
 plt.figure(10)
 inv = np.loadtxt('inverse.dat', unpack=True)
@@ -28,7 +34,9 @@ plt.plot(inv[0], inv[2], label=r'$\rho$')
 # plt.axhline(28, ls='--')
 plt.plot(inv[0], inv[3], label=r'$\beta$')
 # plt.axhline(8/3, ls='--')
-plt.legend()
+plt.legend(fontsize=14)
+plt.xlabel('Epochs')
+plt.ylabel('Coefficients')
 
 # Orders of magnitude
 # plt.figure(10)
